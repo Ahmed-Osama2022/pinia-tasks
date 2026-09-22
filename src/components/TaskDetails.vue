fc74973af0dbaaf895d26be6152d4a2732b2e047
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-09-22 20:22:22 +0300

    refactor TaskDetails component for improved layout and conditional styling

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -8,10 +8,13 @@ defineProps({
 </script>
 
 <template>
-  <div class="card shadow-sm mb-3">
-    <div class="card-body">
-      <span class="badge bg-secondary me-2">#{{ task.id }}</span>
-      <h5 class="card-title d-inline">{{ task.title }}</h5>
+  <div :class="`card shadow-sm mb-3 ${task.isFav ? 'border-primary border-2' : ''}`">
+    <div class="card-body d-flex justify-content-between align-items-center">
+      <div>
+        <span class="badge bg-secondary me-2">#{{ task.id }}</span>
+        <h5 class="card-title d-inline">{{ task.title }}</h5>
+      </div>
+      <span v-if="task.isFav" class="badge text-bg-primary">Favorite</span>
     </div>
   </div>
 </template>
